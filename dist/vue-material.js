e0e1811a0bbcf21b0e9869f61843b1ce93507979
Dmytro Buzinov <dbuzinov@gmail.com>
2022-08-29 15:43:18 +0200

    Cleanup

--- dist/vue-material.js
+++ dist/vue-material.js
@@ -8953,9 +8953,6 @@ exports.default = {
       this.setOffsets();
       this.MdField.focused = true;
       this.$emit('md-opened');
-      // setTimeout(() => {
-      //   window.dispatchEvent(new Event('resize'))
-      // }, 50)
     },
     applyHighlight: function applyHighlight() {
       this.MdField.focused = false;
